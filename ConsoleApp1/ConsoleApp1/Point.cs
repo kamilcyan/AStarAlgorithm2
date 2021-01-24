b3b4ed869be14e8250605de9351fe93b8f1e7eee
kamilcyan <kamil.lucjanek@wp.pl>
2021-01-24 17:23:18 +0100

    first

---
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        Point(float a, float b)
+        {
+            X = a;
+            Y = b;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            int[,] tab = new int[20, 20];
+            tab = prog.GenerujTablice();
+
+            prog.Draw(prog.GenerujTablice());
+
+
+            Console.ReadKey();
+        }
+
+        public int[,] GenerujTablice()
+        {
+            int[,] numery = new int[20, 20];
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    numery[i, j] = random.Next(0, 2);
+                }
+            }
+
+            return numery;
+        }
+
+        public void Draw(int[,] table)
+        {
+            for(int i = 0; i<table.Length; i++)
+            {
+                for(int j = 0; j< table.Length; j++)
+                {
+                    Console.Write(table[i, j]);
+                }
+                Console.Write("/n");
+            }
+        }
+    }
+}
